e40c2840de6d317bd378862ea11c830748a071be
Daffdhot29 <daff.yunus05@gmail.com>
2026-04-11 15:44:47 +0700

    push

--- classify.py
+++ classify.py
@@ -1,6 +1,26 @@
 import mlflow 
+import pandas as pd 
 import time
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
-import pandas as pd 
 from xgboost import XGBClassifier
+
+
+mlflow.set_tracking_uri('http://127.0.0.1:5000/')
+mlflow.set_experiment('Classification-for-Attrition-worker-by-AI')
+
+data = pd.read_csv('../data/ai_worker_burnout_attrition_2026.csv')
+
+X = data.drop(columns=['attrition_risk'], axis=1)
+y = data['attrition_risk']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+input_example = data[0:10]
+
+with mlflow.start_run() : 
+    
