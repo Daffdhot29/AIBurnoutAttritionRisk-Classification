4f6eed2e11ccac5b40feb898c0e7c36884814a51
Daffdhot29 <daff.yunus05@gmail.com>
2026-04-11 15:51:26 +0700

    push

--- classify.py
+++ classify.py
@@ -11,10 +11,21 @@ mlflow.set_experiment('Classification-for-Attrition-worker-by-AI')
 
 data = pd.read_csv('../data/ai_worker_burnout_attrition_2026.csv')
 
+# encoder
+
+le = LabelEncoder()
+cat_features = data.select_dtypes(include=['object']).columns 
+
+for kol in cat_features : 
+    data[kol] = le.fit_transform(data[kol])
+data.head()
+
 X = data.drop(columns=['attrition_risk'], axis=1)
 y = data['attrition_risk']
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+
+
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
@@ -23,4 +34,8 @@ X_test_scaled = scaler.transform(X_test)
 input_example = data[0:10]
 
 with mlflow.start_run() : 
+    # log parameters 
+    n_estimators = 505
+
     
+    mlflow.autolog()
